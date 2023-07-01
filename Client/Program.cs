9cf84aeadb5ec07b206d6dc387304b39a3f9aecf
Michał Sykutera <msykutera@gmail.com>
2023-07-01 10:56:49 +0200

    Cleanup and launch settings fix

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7024");
+var client = new Greeter.GreeterClient(channel);
+
+var request = new HelloRequest { Name = "Michal" };
+var result = await client.SayHelloAsync(request);
+
+Console.WriteLine(result?.Message);
